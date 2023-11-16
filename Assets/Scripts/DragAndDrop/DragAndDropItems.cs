88ce864c3fcfbc005ea240d94ff004a3ba0ba196
platelen <platelen@gmail.com>
2023-11-16 14:32:00 +0300

    Mixing_1
    Добавил добавление элементов в класс для смешивания.

--- Assets/Scripts/DragAndDrop/DragAndDropItems.cs
+++ Assets/Scripts/DragAndDrop/DragAndDropItems.cs
@@ -21,14 +21,5 @@ namespace DragAndDrop
             Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition - _mousePos);
             transform.position = new Vector3(newPosition.x, newPosition.y, transform.position.z);
         }
-
-        private void OnTriggerEnter(Collider other)
-        {
-            if (other.gameObject.layer == LayerMask.NameToLayer("Flask"))
-            {
-                // Обработка соприкосновения с объектом на слое Flask
-                Debug.Log("Trigger Enter with Flask: " + other.gameObject.name);
-            }
-        }
     }
 }
--- Assets/Scripts/Interaction/ChemicalCombination.cs
+++ Assets/Scripts/Interaction/ChemicalCombination.cs
@@ -1,13 +1,56 @@
+using System.Collections;
 using System.Collections.Generic;
 using SO;
 using UnityEngine;
 
 namespace Interaction
 {
-    public class ChemicalCombination:MonoBehaviour
+    public class ChemicalCombination : MonoBehaviour
     {
         [SerializeField] private List<ChemicalCombinationsSo> _chemicalCombinationsSo;
         [SerializeField] private ChemicalData _element1;
         [SerializeField] private ChemicalData _element2;
+
+        private bool _isCombining = false;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!_isCombining && other.gameObject.layer == LayerMask.NameToLayer("Flask"))
+            {
+                ChemicalData chemicalData = other.GetComponent<ChemicalData>();
+                if (chemicalData != null)
+                {
+                    if (_element1 == null)
+                    {
+                        _element1 = chemicalData;
+                    }
+                    else if (_element2 == null && _element1 != chemicalData)
+                    {
+                        _element2 = chemicalData;
+
+                        if (_element1 != null && _element2 != null)
+                        {
+                            StartCoroutine(CombineElements());
+                        }
+                    }
+                }
+            }
+        }
+
+        private IEnumerator CombineElements()
+        {
+            _isCombining = true;
+
+            // Ваша логика комбинирования элементов
+
+            // Ждем некоторое время
+            yield return new WaitForSeconds(2f);
+
+            // Освобождаем поля
+            _element1 = null;
+            _element2 = null;
+
+            _isCombining = false;
+        }
     }
 }
